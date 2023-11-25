6e5eb8e90cabf7907c55e8d6d86ee777ccb15777
vismaya74 <115627302+vismaya74@users.noreply.github.com>
2023-11-25 06:24:24 +0530

    Update proposed_scheme.py

--- src/encryption_scheme/proposed_scheme.py
+++ src/encryption_scheme/proposed_scheme.py
@@ -10,31 +10,29 @@ start_memory = psutil.virtual_memory().used
 start_time = time.time()
 
 # Reading the encryption key
-with open("keystream.txt", 'rb') as key_file:      
+with open(r"C:\Users\visma\OneDrive\Documents\AMRITA-MTECH\code_minor_project\src\encryption_scheme\keystream.txt", 'rb') as key_file:      
     keystream = [int(float(num)) for line in key_file for num in line.split()]
 key_bytes = bytes(keystream[:32])
 # IV generation
 iv = os.urandom(16)
 # Opening image 
-image_path = "128 x 128.png"
-original_image = Image.open(image_path)
-image_bytes = original_image.tobytes()
-
-#encryption
+image_path = r"C:\Users\visma\OneDrive\Documents\AMRITA-MTECH\code_minor_project\src\image_dataset\128 x 128.png"
+plain_image = Image.open(image_path)
+image_bytes = plain_image.tobytes()
+#cipher object created
 chacha20_cipher = Cipher(algorithms.ChaCha20(key_bytes, iv),mode=None)
+#encryption
 chacha20_encryptor = chacha20_cipher.encryptor()
 encrypted_image_bytes = chacha20_encryptor.update(image_bytes) + chacha20_encryptor.finalize()
-encrypted_image = Image.frombytes('RGB', original_image.size, encrypted_image_bytes)
-encrypted_image_path = "128_encrypted.png"
+encrypted_image = Image.frombytes('RGB', plain_image.size, encrypted_image_bytes)
+encrypted_image_path = r"C:\Users\visma\OneDrive\Documents\AMRITA-MTECH\code_minor_project\src\encryption_scheme\128_encrypted.png"
 encrypted_image.save(encrypted_image_path)
 print("Encrypted Image Saved at:", encrypted_image_path)
-
 #decryption
-chacha20_cipher = Cipher(algorithms.ChaCha20(key_bytes, iv), mode=None)
 chacha20_decryptor = chacha20_cipher.decryptor()
 decrypted_image_bytes = chacha20_decryptor.update(encrypted_image_bytes) + chacha20_decryptor.finalize()
 decrypted_image = Image.frombytes('RGB', encrypted_image.size, decrypted_image_bytes)
-decrypted_image_path = "decrypted_128.png"
+decrypted_image_path = r"C:\Users\visma\OneDrive\Documents\AMRITA-MTECH\code_minor_project\src\encryption_scheme\128_decrypted.png"
 decrypted_image.save(decrypted_image_path)
 print("Decrypted Image Saved at:", decrypted_image_path)
 
